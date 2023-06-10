a9eaea7674445015a35f05ee469fd43acfccdf2f
Ilya Bylich <ibylich@gmail.com>
2023-06-10 20:08:32 +0200

    bind to 0.0.0.0

--- src/web.rs
+++ src/web.rs
@@ -12,7 +12,7 @@ impl Web {
             .route("/preview", get(Self::preview))
             .with_state(state);
 
-        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+        let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
         println!("Listening on {}", addr);
 
         Server::bind(&addr)
