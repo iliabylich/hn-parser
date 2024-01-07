52de30a013d2a874633c731da2aa47af4d5a8191
Ilya Bylich <ibylich@gmail.com>
2024-01-07 20:11:35 +0100

    style change styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.html"],
+  content: ['./views/*.html'],
   theme: {
     extend: {},
   },
   plugins: [],
+  mode: 'jit',
 }
